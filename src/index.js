7b0ee51720551525711f75c0f9432e6b93dbf410
Matheus Ferreira <math.ferr@outlook.com>
2021-03-24 09:47:25 -0300

    Init do express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Lorem Ipsum");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send({ lorem: "Ipsum" });
+});
+
+app.listen(4000, () => {
+  console.log("Server is up and listening on port 4000");
+});
