7a8165ac9b6b305475b76b3c07947a5052f7d42e
Matheus Ferreira <math.ferr@outlook.com>
2021-03-24 13:12:05 -0300

    Init dos middlewares

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const cors = require("cors");
+const helmet = require("helmet");
 const routes = require("./routes");
 
 const app = express();
 
+app.use(morgan("combined"));
+
 app.use(express.json());
+
+app.use(compression());
+
+app.use(helmet());
+
+app.use(
+  cors({
+    /*
+     *  Substituir origin com a URL do front;
+     */
+    origin: "*",
+  })
+);
 
 app.use(routes);
 
