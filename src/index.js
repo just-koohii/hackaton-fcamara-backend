804ce7fb0b112078206d0d1495d53a78413d831f
Matheus Ferreira <math.ferr@outlook.com>
2021-04-01 13:35:34 -0300

    dotenv files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("module-alias/register");
 const app = require("./app");
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log("Server is up and listening on port 4000");
 });
